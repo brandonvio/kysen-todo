8e00e97dfeefe4c92882584cef7df1be44c26357
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-10-03 15:13:59 -0700

    Kysen TODO dev.

--- cdk-app/bin/cdk-pipelines.ts
+++ cdk-app/bin/cdk-pipelines.ts
@@ -5,7 +5,7 @@ import { CdkAppPipelineStack } from "../lib/cdk-app-pipeline-stack";
 const app = new App();
 
 new CdkAppPipelineStack(app, "CdkAppPipelineStack", {
-  env: { account: "780883706235", region: "us-west-2" },
+  env: { account: "705871014762", region: "us-west-2" },
 });
 
 app.synth();
