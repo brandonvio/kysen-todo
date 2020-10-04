cfe514d3182f7d5220f22e56bf6ce8cfa2555397
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-10-04 13:49:21 -0700

    Kysen TODO dev.

--- cdk-app/build.sh
+++ cdk-app/build.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
-npm install
-npm install -g webpack-cli
-npm install -g typescript
-ls -al
+# npm install
+# npm install -g webpack-cli
+# npm install -g typescript
+# ls -al
 
-echo "Building lambda."
+echo "Building lambda app."
 echo $PWD
 cd ..
 echo $PWD
 cd lambda-app
 echo $PWD
 npm install
-tsc
-webpack -p
-echo "Done building lambda."
+npm run build
+echo "Done building lambda app."
 
+echo "Building react app."
+echo $PWD
 cd ..
 echo $PWD
-cd cdk-app
+cd react-app
 echo $PWD
-tsc
+npm install
+npm run build
+echo "Done building react app."
+
+# cd ..
+# echo $PWD
+# cd cdk-app
+# echo $PWD
+# tsc
--- cdk-app/lib/todo-cdk-pipeline-stack.ts
+++ cdk-app/lib/todo-cdk-pipeline-stack.ts
@@ -1,6 +1,5 @@
 import * as codepipeline from "@aws-cdk/aws-codepipeline";
 import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
-import * as codebuild from "@aws-cdk/aws-codebuild";
 import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
 import { CdkAppStage1 } from "./cdk-app-stage-1";
@@ -11,111 +10,33 @@ export class TodoCdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const sourceArtifact = new codepipeline.Artifact("todoSourceArtifact");
-    const cloudAssemblyArtifact = new codepipeline.Artifact("todoCloudAssemblyArtifact");
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
 
-    const cdkBuildOutput = new codepipeline.Artifact("CdkBuildOutput");
-    const lambdaBuildOutput = new codepipeline.Artifact("LambdaBuildOutput");
-    const reactBuildOuput = new codepipeline.Artifact("ReactBuildOuput");
+    const pipeline = new CdkPipeline(this, "KysenTodoPipeline", {
+      // The pipeline name
+      pipelineName: "KysenTodoPipeline",
+      cloudAssemblyArtifact,
 
-    const todoCodePipeline = new codepipeline.Pipeline(this, "KysenBuildTodoCodePipeline", {
-      pipelineName: "KysenBuildTodoCodePipeline",
-    });
+      // Where the source can be found
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: "GitHub",
+        output: sourceArtifact,
+        oauthToken: SecretValue.secretsManager("github-token"),
+        owner: "brandonvio",
+        repo: "kysen-todo",
+      }),
 
-    // Source.
-    const sourceAction = new codepipeline_actions.GitHubSourceAction({
-      actionName: "GitHub",
-      owner: "brandonvio",
-      repo: "kysen-todo",
-      oauthToken: SecretValue.secretsManager("github-token"),
-      output: sourceArtifact,
-      branch: "master", // default: 'master'
-    });
-
-    todoCodePipeline.addStage({
-      stageName: "Source",
-      actions: [sourceAction],
-    });
-
-    // Build Lambda
-    const lambdaProject = new codebuild.PipelineProject(this, "lambdaProject", {
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: "0.2",
-        env: {
-          "exported-variables": ["MY_VAR"],
-        },
-        phases: {
-          pre_build: {
-            commands: ["cd lambda-app", "npm install"],
-          },
-          build: {
-            commands: ["npm run build", "ls -al", "pwd"],
-          },
-        },
+      // How it will be built and synthesized
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        subdirectory: "cdk-app",
+        buildCommand: "npm run build",
       }),
     });
 
-    const buildLambdaAction = new codepipeline_actions.CodeBuildAction({
-      actionName: "CodeBuild",
-      project: lambdaProject,
-      input: sourceArtifact,
-      outputs: [lambdaBuildOutput],
-    });
-
-    todoCodePipeline.addStage({
-      stageName: "BuildLambda",
-      actions: [buildLambdaAction],
-    });
-
-    // Build React
-    const reactProject = new codebuild.PipelineProject(this, "reactProject", {
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: "0.2",
-        env: {
-          "exported-variables": ["MY_VAR"],
-        },
-        phases: {
-          pre_build: {
-            commands: ["cd react-app", "npm install"],
-          },
-          build: {
-            commands: ["npm run build", , "ls -al", "pwd"],
-          },
-        },
-      }),
-    });
-
-    const buildReactAction = new codepipeline_actions.CodeBuildAction({
-      actionName: "CodeBuild",
-      project: reactProject,
-      input: sourceArtifact,
-      outputs: [reactBuildOuput],
-    });
-
-    todoCodePipeline.addStage({
-      stageName: "BuildReact",
-      actions: [buildReactAction],
-    });
-
-    // Synth.
-    const synthAction = SimpleSynthAction.standardNpmSynth({
-      sourceArtifact,
-      cloudAssemblyArtifact,
-      subdirectory: "cdk-app",
-    });
-
-    todoCodePipeline.addStage({
-      stageName: "Synth",
-      actions: [synthAction],
-    });
-
-    // CDK Pipeline
-    const cdkPipeline = new CdkPipeline(this, "KysenBuildTodoCDKPipeline", {
-      codePipeline: todoCodePipeline,
-      cloudAssemblyArtifact,
-    });
-
-    cdkPipeline.addApplicationStage(
+    pipeline.addApplicationStage(
       new CdkAppStage1(this, "PreProd", {
         env: { account: "705871014762", region: "us-west-2" },
       })
