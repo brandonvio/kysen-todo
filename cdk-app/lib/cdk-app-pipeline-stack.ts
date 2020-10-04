f8e9fcdb2dffc7da17874ab004ddc62ff59841e2
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-10-03 19:22:49 -0700

    Kysen TODO dev.

--- cdk-app/lib/cdk-app-pipeline-stack.ts
+++ cdk-app/lib/cdk-app-pipeline-stack.ts
@@ -3,7 +3,6 @@ import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
 import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
 import { CdkAppStage1 } from "./cdk-app-stage-1";
-import { ShellScriptAction } from "@aws-cdk/pipelines";
 /**
  * The stack that defines the application pipeline
  */
@@ -32,44 +31,17 @@ export class CdkAppPipelineStack extends Stack {
       synthAction: SimpleSynthAction.standardNpmSynth({
         sourceArtifact,
         cloudAssemblyArtifact,
-        subdirectory: "cdk-app",
         // We need a build step to compile the TypeScript Lambda
         buildCommand: "npm run build",
       }),
     });
 
-    SimpleSynthAction.standardNpmSynth({
-      sourceArtifact,
-      cloudAssemblyArtifact,
-      subdirectory: "lambda-app",
-      // We need a build step to compile the TypeScript Lambda
-      buildCommand: "npm run build",
-    });
-
     // This is where we add the application stages
     // ...
-
-    const preprod = new CdkAppStage1(this, "PreProd", {
-      env: { account: "705871014762", region: "us-west-2" },
-    });
-
-    const preprodStage = pipeline.addApplicationStage(preprod);
-
-    // preprodStage.addActions()
-
-    // preprodStage.addActions(
-    //   new ShellScriptAction({
-    //     actionName: "build lambda-app",
-    //     useOutputs: {
-    //       // Get the stack Output from the Stage and make it available in
-    //       // the shell script as $ENDPOINT_URL.
-    //       ENDPOINT_URL: pipeline.stackOutput(preprod.urlOutput),
-    //     },
-    //     commands: [
-    //       // Use 'curl' to GET the given URL and fail if it returns an error
-    //       "curl -Ssf $ENDPOINT_URL",
-    //     ],
-    //   })
-    // );
+    pipeline.addApplicationStage(
+      new CdkAppStage1(this, "PreProd", {
+        env: { account: "705871014762", region: "us-west-2" },
+      })
+    );
   }
 }
