ce1a7dfd0395b7578cd342cb7b2261394e781d86
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-10-03 19:44:46 -0700

    Kysen TODO dev.

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-echo "Building cdk."
-cd cdk-app
-npm run build
-echo "Done building cdk."
-
 echo "Building lambda."
 cd ..
 cd lambda-app
 npm run build
 echo "Done building lambda."
+
+echo "Building cdk."
+cd cdk-app
+npm run build
+echo "Done building cdk."
 
 cd ..
 cd cdk-app
