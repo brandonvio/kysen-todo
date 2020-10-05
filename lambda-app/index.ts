2c6c048bf3ae0110d71c2adaca1122f908bf3577
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-10-04 22:27:41 -0700

    Kysen TODO dev.

--- lambda-app/index.ts
+++ lambda-app/index.ts
@@ -2,10 +2,15 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 import { DbService } from "./services/DbService";
 
 const dbService = new DbService();
-
+const corsHeaders = {
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Origin": "https://www.example.com",
+  "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+};
 export async function defaultTodoHandler(event: APIGatewayProxyEvent, context: Context) {
   console.log(event);
   return {
+    headers: corsHeaders,
     body: "defaultTodoHandler",
     statusCode: 200,
   };
@@ -14,6 +19,7 @@ export async function defaultTodoHandler(event: APIGatewayProxyEvent, context: C
 export async function testHandler(event: APIGatewayProxyEvent, context: Context) {
   console.log(event);
   return {
+    headers: corsHeaders,
     body: "testHandler",
     statusCode: 200,
   };
@@ -24,12 +30,14 @@ export async function getTodosHandler(event: APIGatewayProxyEvent, context: Cont
     console.log(event);
     const result = await dbService.getTodos("brandonv");
     return {
+      headers: corsHeaders,
       body: JSON.stringify(result.Items),
       statusCode: 200,
     };
   } catch (error) {
     console.log(error);
     return {
+      headers: corsHeaders,
       body: JSON.stringify(error),
       statusCode: 500,
     };
@@ -41,11 +49,13 @@ export async function saveTodoHandler(event: any, context: Context) {
     console.log(event);
     const result = await dbService.saveTodos(JSON.parse(event.body));
     return {
+      headers: corsHeaders,
       statusCode: 200,
     };
   } catch (error) {
     console.log(error);
     return {
+      headers: corsHeaders,
       body: JSON.stringify(error),
       statusCode: 500,
     };
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,46 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import { Navbar } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <>
+        <Navbar bg="light">
+          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+        </Navbar>
+        <br />
+        <Navbar bg="light">
+          <Navbar.Brand>Brand text</Navbar.Brand>
+        </Navbar>
+        <br />
+        <Navbar bg="dark">
+          <Navbar.Brand href="#home">
+            <img
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Navbar>
+        <br />
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            React Bootstrap
+          </Navbar.Brand>
+        </Navbar>
+      </>
       <TodoList />
     </div>
   );
