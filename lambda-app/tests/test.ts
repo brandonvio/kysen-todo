e580bfddcd1ac8c4727951e29c4607816e93f370
Brandon <brandonvio@outlook.com>
2020-10-06 11:09:45 -0700

    Kysen TODO dev.

--- lambda-app/tests/test.ts
+++ lambda-app/tests/test.ts
@@ -1,14 +1,14 @@
 import { getExpireEpoch } from "../services/common";
 
 test("test getExpireEpoch", () => {
-  const x1 = getExpireEpoch(0, new Date("July 1, 1978 02:30:00"));
+  const x1 = getExpireEpoch(0, new Date("1978-07-01T09:30:00.000Z"));
   expect(x1).toBe(268133400);
 
-  const x2 = getExpireEpoch(1, new Date("July 1, 1978 02:30:00"));
+  const x2 = getExpireEpoch(1, new Date("1978-07-01T09:30:00.000Z"));
   const x3 = x2 - x1;
   expect(x3).toBe(86400);
 
-  const x4 = getExpireEpoch(26, new Date("July 1, 1978 02:30:00"));
+  const x4 = getExpireEpoch(26, new Date("1978-07-01T09:30:00.000Z"));
   const x5 = x4 - x1;
   expect(x5).toBe(86400 * 26);
 });
--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import "mutationobserver-shim";
+global.MutationObserver = window.MutationObserver;
 
-test('renders learn react link', () => {
+test("renders the todo application", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/When does it need to be done?/i);
   expect(linkElement).toBeInTheDocument();
 });
