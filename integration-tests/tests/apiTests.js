d7cb04fc57054fc9c1a4c169cd73ee8a3a79f816
Brandon <brandonvio@outlook.com>
2020-10-06 19:36:09 -0700

    Kysen TODO dev.

--- integration-tests/tests/apiTests.js
+++ integration-tests/tests/apiTests.js
@@ -1,5 +1,26 @@
 const axios = require("axios");
+const apiUrl = "https://dzun420jh3.execute-api.us-west-2.amazonaws.com/prod/todos";
+const uuid = require("uuid");
 
-test("Should post record to api.", () => {
+test("Should post record to api.", async () => {
   console.log("passed.");
+  console.log(uuid.v4());
+
+  const createdDate = "2020-10-07T01:03:09.040Z";
+  const dueDate = "2020-10-07T08:03:09.040Z";
+
+  const pk = uuid.v4();
+  const sk = uuid.v4();
+
+  const postData = {
+    pk: pk,
+    sk: sk,
+    createdDate: createdDate,
+    dueDate: dueDate,
+    description: "This todo is created as an integration test.",
+    todoState: "pending",
+  };
+
+  const response = await axios.post(apiUrl, postData);
+  expect(response.status).toBe(200);
 });
