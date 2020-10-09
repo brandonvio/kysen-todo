7c15fe5d9ac09c00a15bf69031b09491d1659b2b
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-10-08 19:49:16 -0700

    Kysen TODO dev.

--- react-app/src/actions/todoActions.js
+++ react-app/src/actions/todoActions.js
@@ -1,29 +1,75 @@
 import axios from "axios";
+import moment from "moment";
 import { fieldSorter } from "../common";
 const todoUrl = "https://dzun420jh3.execute-api.us-west-2.amazonaws.com/prod/todos";
 
 const getTodos = () => {
   return async (dispatch) => {
-    console.log("getting data...");
-    const config = {
-      headers: {
-        Authorization: "brandonv",
-      },
-    };
-    const result = await axios.get(todoUrl, config);
-    const todoData = result.data.sort(fieldSorter(["-todoState", "dueDate"]));
-    return dispatch({
-      type: "GET_TODOS",
-      payload: todoData,
-    });
+    try {
+      console.log("getting data...");
+      const config = {
+        headers: {
+          Authorization: "brandonv",
+        },
+      };
+      const result = await axios.get(todoUrl, config);
+      const todoItems = result.data.sort(fieldSorter(["-todoState", "dueDate"]));
+      const payload = {
+        todoItems,
+        relativeTimes: getRelativeTimes(),
+      };
+      return dispatch({
+        type: "GET_TODOS",
+        payload,
+      });
+    } catch (error) {
+      console.error(error);
+      return Promise.reject("There was an error getting the todos from the API.");
+    }
   };
 };
 
 const saveTodo = (todoItem) => {
   return async (dispatch) => {
-    await axios.post(todoUrl, todoItem);
-    return dispatch(getTodos());
+    try {
+      await axios.post(todoUrl, todoItem);
+      return dispatch(getTodos());
+    } catch (error) {
+      console.error(error);
+      return Promise.reject("There was an error saving the todo to the API.");
+    }
   };
+};
+
+const getRelativeTimes = () => {
+  const relativeTimes = [
+    {
+      description: "Now (ish)...",
+      when: moment().add(1, "hours").fromNow(),
+      value: moment().add(1, "hours").toISOString(),
+    },
+    {
+      description: "Today...",
+      when: moment().endOf("day").fromNow(),
+      value: moment().endOf("day").toISOString(),
+    },
+    {
+      description: "Tomorrow...",
+      when: moment().add(1, "days").fromNow(),
+      value: moment().add(1, "days").toISOString(),
+    },
+    {
+      description: "This week...",
+      when: moment().add(3, "days").fromNow(),
+      value: moment().add(3, "days").toISOString(),
+    },
+    {
+      description: "Next week...",
+      when: moment().add(7, "days").fromNow(),
+      value: moment().add(7, "days").toISOString(),
+    },
+  ];
+  return relativeTimes;
 };
 
 export default {
--- react-app/src/reducers/todoReducer.js
+++ react-app/src/reducers/todoReducer.js
@@ -1,54 +1,21 @@
-import moment from "moment";
-
 const todoReducer = (state = { todos: [], relativeTimes: [] }, action) => {
   //   console.log(state, action);
   switch (action.type) {
     case "GET_TODOS":
       return {
         ...state,
-        todos: action.payload,
-        relativeTimes: getRelativeTimes(),
+        todos: action.payload.todoItems,
+        relativeTimes: action.payload.relativeTimes,
       };
     case "SAVE_TODO":
       return {
         ...state,
-        todos: action.payload,
-        relativeTimes: getRelativeTimes(),
+        todos: action.payload.todoItems,
+        relativeTimes: action.payload.relativeTimes,
       };
     default:
       return state;
   }
-};
-
-const getRelativeTimes = () => {
-  const relativeTimes = [
-    {
-      description: "Now (ish)...",
-      when: moment().add(1, "hours").fromNow(),
-      value: moment().add(1, "hours").toISOString(),
-    },
-    {
-      description: "Today...",
-      when: moment().endOf("day").fromNow(),
-      value: moment().endOf("day").toISOString(),
-    },
-    {
-      description: "Tomorrow...",
-      when: moment().add(1, "days").fromNow(),
-      value: moment().add(1, "days").toISOString(),
-    },
-    {
-      description: "This week...",
-      when: moment().add(3, "days").fromNow(),
-      value: moment().add(3, "days").toISOString(),
-    },
-    {
-      description: "Next week...",
-      when: moment().add(7, "days").fromNow(),
-      value: moment().add(7, "days").toISOString(),
-    },
-  ];
-  return relativeTimes;
 };
 
 export default todoReducer;
