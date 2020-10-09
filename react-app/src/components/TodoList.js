5b6dc97b75565cc4b0bfaf5f3aa3cc0b588c0dcb
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-10-08 19:30:17 -0700

    Kysen TODO dev.

--- react-app/src/components/TodoList.js
+++ react-app/src/components/TodoList.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { ListGroup, Button } from "react-bootstrap";
-import moment from "moment";
+import { ListGroup } from "react-bootstrap";
 import allActions from "../actions";
+import TodoItem from "./TodoItem";
 
 /**
  * TodoList
@@ -16,53 +16,11 @@ export default function TodoList({ todoItems }) {
     };
     fetchData();
   }, [dispatch]);
-  const updateItem = async (todoItem) => {
-    dispatch(allActions.todoActions.saveTodo(todoItem));
-  };
   return (
     <ListGroup variant="flush">
       {todoItems.map((item) => (
         <ListGroup.Item key={item.sk}>
-          <div>
-            <div className="form-check">
-              <input
-                type="checkbox"
-                id={item.sk}
-                className="form-check-input"
-                checked={item.todoState === "done"}
-                data-keeper-edited="yes"
-                onChange={(e) => {
-                  if (item.todoState === "pending") {
-                    item.todoState = "done";
-                  } else {
-                    item.todoState = "pending";
-                  }
-                  updateItem(item);
-                }}
-              />
-              <label htmlFor={item.sk} className={item.todoState === "done" ? "line-through" : ""}>
-                {item.description}
-              </label>
-            </div>
-            <div className={item.todoState === "done" ? "line-through" : ""}>
-              <span style={{ color: "lightblue" }}>Created</span>{" "}
-              {moment(item.createdDate).fromNow()}
-              {", "}
-              <span style={{ color: "lightblue" }}>due</span> {moment(item.dueDate).fromNow()}.
-            </div>
-          </div>
-          <div>
-            <Button
-              style={{ margin: "0px", padding: "0px" }}
-              variant="link"
-              onClick={(e) => {
-                item.todoState = "archived";
-                updateItem(item);
-              }}
-            >
-              archive
-            </Button>
-          </div>
+          <TodoItem item={item} />
         </ListGroup.Item>
       ))}
     </ListGroup>
