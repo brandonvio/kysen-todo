40dcb55b2799ddc15597c05d78a389de06cd9bea
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-11-19 16:37:14 -0800

    Kysen TODO dev.

--- react-app/src/components/AppNavbar.js
+++ react-app/src/components/AppNavbar.js
@@ -7,7 +7,9 @@ import { Navbar } from "react-bootstrap";
 export function AppNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">Todo... an app for Kysen Build</Navbar.Brand>
+      <Navbar.Brand href="#home">
+        Todo... <a href="">...a portfolio application</a>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
     </Navbar>
   );
