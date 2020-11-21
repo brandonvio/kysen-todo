31da669b4b791f64cf74c6e634f265e21235ef38
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-11-21 13:18:41 -0800

    Kysen TODO dev.

--- build.sh
+++ build.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
 echo  "Change directory to react-app."
-cd react-app || exit 0
+cd react-app || exit -1
 
 echo  "Run react build."
-npm install || exit 0
-npm run build || exit 0
+npm install || exit -1
+npm run build || exit -1
 
 echo  "Return to the root directory."
 cd ..
 
 echo  "Copy result of build to the CDK project for deployment."
-cp -r ./react-app--xxxx/build ./cdk-app-02/builds/react-app-build || exit 0
+cp -r ./react-app--xxxx/build ./cdk-app-02/builds/react-app-build || exit -1
 
 echo  "Change directory to lambda-app."
-cd lambda-app || exit 0
+cd lambda-app || exit -1
 
 echo  "Compile typescript."
-tsc --extendedDiagnostics || exit 0
+tsc --extendedDiagnostics || exit -1
 
 echo  "Run build, which runs webpack."
-npm install || exit 0
-npm run build || exit 0
+npm install || exit -1
+npm run build || exit -1
 
 echo  "Return to the root directory."
 cd ..
 
 echo  "Copy result of build to the CDK project for deployment."
-cp -r ./lambda-app/build ./cdk-app-02/builds/lambda-app-build || exit 0
+cp -r ./lambda-app/build ./cdk-app-02/builds/lambda-app-build || exit -1
 
 echo  "Change directory to ipep-cdk."
-cd cdk-app-02 || exit 0
+cd cdk-app-02 || exit -1
 
 echo  "Run cdk deploy."
-npm install || exit 0
-npm run build || exit 0
-cdk synth || exit 0
+npm install || exit -1
+npm run build || exit -1
+cdk synth || exit -1
 # cdk deploy "*" --profile kysen-build-dev
 
 echo  "Return to the root directory."
