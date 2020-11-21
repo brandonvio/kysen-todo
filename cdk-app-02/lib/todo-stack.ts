acccfef38b5127831e07f76aa68192a18019ebf9
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-11-21 15:44:17 -0800

    Kysen TODO dev.

--- cdk-app-02/lib/todo-stack.ts
+++ cdk-app-02/lib/todo-stack.ts
@@ -1,44 +1,46 @@
 import * as cdk from "@aws-cdk/core";
-import { TodoApiStack } from "../lib/todo-api-stack"
-import { TodoLambdaStack, ILambdaFunctions } from "../lib/lambda-stack"
-import { TodoDynamodbStack } from "../lib/dynamodb-stack"
-import { TodoWebsiteStack } from "../lib/website-stack"
+import { TodoApiStack } from "../lib/todo-api-stack";
+import { TodoLambdaStack, ILambdaFunctions } from "../lib/lambda-stack";
+import { TodoDynamodbStack } from "../lib/dynamodb-stack";
+import { TodoWebsiteStack } from "../lib/website-stack";
+import { TodoCognitoStack } from "../lib/cognito-stack";
 // import { TodoApiStack } from "../lib/todo-api-stack"
 
 /**
  * A stack for the Todo API.
  */
 export class TodoStack extends cdk.Stack {
-
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaStack = new TodoLambdaStack(scope, "TodoLambdaStack",  {
-      stackName: "TodoLambdaStack",
-      env: props?.env
+    const cognitoStack = new TodoCognitoStack(scope, "TodoCognitoStack", {
+      stackName: "TodoCognitoStack",
+      env: props?.env,
     });
 
-    const apiStack = new TodoApiStack(
-        scope, 
-        "TodoApiStack", 
-        lambdaStack.lambdaFunctions, {
-          stackName: "TodoApiStack",
-          env: props?.env
-        });
+    const lambdaStack = new TodoLambdaStack(scope, "TodoLambdaStack", {
+      stackName: "TodoLambdaStack",
+      env: props?.env,
+    });
+
+    const apiStack = new TodoApiStack(scope, "TodoApiStack", lambdaStack.lambdaFunctions, {
+      stackName: "TodoApiStack",
+      env: props?.env,
+    });
 
     const dynamodbStack = new TodoDynamodbStack(
-      scope, 
-      "TodoDynamodbStack", 
-      lambdaStack.lambdaFunctions, {
+      scope,
+      "TodoDynamodbStack",
+      lambdaStack.lambdaFunctions,
+      {
         stackName: "TodoDynamodbStack",
-        env: props?.env
-      });
+        env: props?.env,
+      }
+    );
 
-    const websiteStack = new TodoWebsiteStack(
-      scope, 
-      "TodoWebsiteStack", {
-        stackName: "TodoWebsiteStack",
-        env: props?.env
-      });
+    const websiteStack = new TodoWebsiteStack(scope, "TodoWebsiteStack", {
+      stackName: "TodoWebsiteStack",
+      env: props?.env,
+    });
   }
 }
