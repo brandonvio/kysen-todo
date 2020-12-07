dd566c87ceadc6831bfad1d24c3487e258add990
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-12-07 12:45:06 -0800

    Updated signup page.

--- react-app/src/components/auth/SignupPage.js
+++ react-app/src/components/auth/SignupPage.js
@@ -34,10 +34,22 @@ export default function SignupPage() {
           <FormControl name="email" placeholder="email" ref={register} required />
         </InputGroup>
         <InputGroup className="mb-3">
-          <FormControl name="phone_number" placeholder="phone" ref={register} required />
+          <FormControl
+            placeholder="+15551230000"
+            name="phone_number"
+            placeholder="phone"
+            ref={register}
+            required
+          />
         </InputGroup>
         <InputGroup className="mb-3">
-          <Form.Control name="password" placeholder="password" ref={register} required />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="password"
+            ref={register}
+            required
+          />
         </InputGroup>
         <InputGroup>
           <Button type="submit" variant="info">
